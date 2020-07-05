cd2fbb79703d28479904390ecf59e8a6e4d570f4
priyasankar21 <priyasankar1872@gmail.com>
2020-07-05 21:14:07 +0530

    assignment completed

---
+++ javas/script.js
@@ -0,0 +1,12 @@
+//var string="Hello ";
+//string +="World";
+//console.log(string + "!");
+
+
+console.log((5 + 4)/3);
+//console.log(undefined / 5 );
+
+//var x=4,y=4;
+//if(x==y) {
+	console.log("x=4 is equal to y=4")
+}
